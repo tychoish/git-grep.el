7f942089808fa26669c207a7a796d2ccb14c41ac
tycho garen <garen@tychoish.com>
2020-09-19 13:32:51 -0400

    improve documentation

--- git-grep.el
+++ git-grep.el
@@ -24,7 +24,22 @@
 
 ;;; Commentary:
 
-;; commentary
+;; Provides an alternative git-grep based project search using git
+;; grep as an alternative to `vc-git-grep' and various ripgrep and
+;; silver searcher alternatives.  `git grep' is noticeably faster than
+;; find+grep, and comes with git, so it's always available without
+;; additional dependencies.  Where `vc-git-grep' includes a number of
+;; prompts for various options (including extension's and project
+;; path), these commands default to searching all files in the
+;; repository or directory tree and work with a single command.
+
+;; I use the following configuration to add key bindings for these
+;; grep operations:
+
+;;   (use-package git-grep
+;;     :commands (git-grep git-grep-repo)
+;;     :bind (("C-c g g" . git-grep)
+;;            ("C-c g r" . git-grep-repo)))
 
 ;;; Code:
 
@@ -44,13 +59,15 @@
     (grep-apply-setting 'grep-find-template old-command)))
 
 (defun git-grep-repo ()
-  "Search for the given regexp using `git grep' in the current directory."
+  "Search for the given regexp using `git grep' in the entire repository.
+This operation uses projectile to determine the root of the project
+and, by default, searchers for the current selection.."
   (interactive)
   (unless (boundp 'grep-find-template) (grep-compute-defaults))
   (let ((old-command grep-find-template)
 	(search-text (buffer-substring-no-properties (region-beginning) (region-end))))
     (message search-text)
-    (grep-apply-setting 'grep-find-template "git --no-pager grep --no-color --line-number <C> <R>")
+    (grep-apply-setting 'grep-find-template git-grep-command)
     (rgrep search-text "*" (projectile-project-root))
     (grep-apply-setting 'grep-find-template old-command)))
 
